e5d0296515a0f8ee62da3ddaf3aa94020cf70606
gZanda <gabrielfzanda@gmail.com>
2024-05-07 11:16:46 -0300

    Lombok

--- API/src/main/java/com/Springzada/API/model/FazendaModel.java
+++ API/src/main/java/com/Springzada/API/model/FazendaModel.java
@@ -5,49 +5,26 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-import java.io.Serializable;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name="FAZENDA")
-public class FazendaModel implements Serializable {
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class FazendaModel{
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
+
+    @NotNull
     private String nome;
+
+    @NotNull
     private float area;
-    
-    // Constructors
-
-    public FazendaModel() {
-    }
-
-    public FazendaModel(int id, String nome, Long area) {
-        this.id = id;
-        this.nome = nome;
-        this.area = area;
-    }
-
-    // Getters and Setters
-
-    public int getId() {
-        return id;
-    }
-    public void setId(int id) {
-        this.id = id;
-    }
-    public String getNome() {
-        return nome;
-    }
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-    public float getArea() {
-        return area;
-    }
-    public void setArea(float area) {
-        this.area = area;
-    }
-
 
 }
