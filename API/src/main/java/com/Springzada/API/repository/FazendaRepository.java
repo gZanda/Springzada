22e7aa1ace942d375d641c6a6b4002f1b3114755
gZanda <gabrielfzanda@gmail.com>
2024-05-07 12:09:10 -0300

    JPA fix

--- API/src/main/java/com/Springzada/API/repository/FazendaRepository.java
+++ API/src/main/java/com/Springzada/API/repository/FazendaRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface FazendaRepository extends JpaRepository<FazendaModel, Integer>{
+public interface FazendaRepository extends JpaRepository<FazendaModel, Long>{
 
     FazendaModel findByNome(String nome);
 
